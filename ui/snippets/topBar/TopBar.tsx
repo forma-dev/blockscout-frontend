b9883e447e0a9f87f3ab27e5a8e0d2c641da1a25
Josh Dechant <josh@codecrafting.org>
2025-01-15 17:58:15 -0500

    ui tweaks

--- ui/snippets/topBar/TopBar.tsx
+++ ui/snippets/topBar/TopBar.tsx
@@ -6,7 +6,7 @@ import { CONTENT_MAX_WIDTH } from 'ui/shared/layout/utils';
 
 import DeFiDropdown from './DeFiDropdown';
 import NetworkMenu from './NetworkMenu';
-import Settings from './settings/Settings';
+// import Settings from './settings/Settings';
 import TopBarStats from './TopBarStats';
 
 const TopBar = () => {
@@ -30,7 +30,7 @@ const TopBar = () => {
               <Divider mr={ 3 } ml={{ base: 2, sm: 3 }} height={ 4 } orientation="vertical"/>
             </>
           ) }
-          <Settings/>
+          { /* <Settings/> */ }
           { config.UI.navigation.layout === 'horizontal' && Boolean(config.UI.navigation.featuredNetworks) && (
             <Box display={{ base: 'none', lg: 'flex' }}>
               <Divider mx={ 3 } height={ 4 } orientation="vertical"/>
