8b859e1c44c57d7815091dcf3f314223f65549a7
Josh Dechant <josh@codecrafting.org>
2025-05-02 15:48:17 -0400

    fix gwei units

--- lib/units.ts
+++ lib/units.ts
@@ -3,9 +3,10 @@ import type { Unit } from 'types/unit';
 import config from 'configs/app';
 
 const weiName = config.chain.currency.weiName || 'wei';
+const symbol = config.chain.currency.symbol || 'ETH';
 
 export const currencyUnits: Record<Unit, string> = {
   wei: weiName,
-  gwei: `G${ weiName }`,
-  ether: config.chain.currency.symbol || 'ETH',
+  gwei: `n${ symbol.toLowerCase() }`,
+  ether: symbol,
 };
